1f990e6897b42e1d7d5bb34a9c29ae794096c342
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-07-16 03:34:02 +0430

    set background

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+import StarBackground from "../Components/StarBackground";
 import ThemeToggle from "../Components/ThemeToggle";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-forground overflow-x-hidden">
       <ThemeToggle />
+      <StarBackground />
     </div>
   );
 }
