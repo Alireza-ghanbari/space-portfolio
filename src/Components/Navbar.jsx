1379153eb220c0907d3e40b71fa8a4ae9954bc12
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-07-16 04:06:28 +0430

    create navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import { cn } from "../lib/utils";
+import { IoMdClose, IoMdMenu } from "react-icons/io";
+
+const navItems = [
+  { name: "Home", href: "#hero" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
+];
+export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <nav
+      className={cn(
+        "fixed w-full z-40 transition-all duration-300",
+        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadowxs " : "py-5"
+      )}
+    >
+      <div className="container flex items-center justify-between">
+        <a
+          href="#hero"
+          className="text-xl font-bold text-primary flex items-center"
+        >
+          <span className="relative z-10">
+            <span className="text-glow text-forground">Alireza</span> Ghanbari
+          </span>
+        </a>
+
+        {/* desktop */}
+        <div className="hidden md:flex space-x-8">
+          {navItems.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-forground/80 hover:text-primary transition-colors duration-300"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+
+        {/* mobile */}
+
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="md:hidden p-2 text-forground z-50"
+        >
+          {isMenuOpen ? <IoMdClose size={24} /> : <IoMdMenu size={24} />}
+        </button>
+
+        <div
+          className={cn(
+            "fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+            "transition-all duration-300 md:hidden",
+            isMenuOpen
+              ? "opacity-100 pointer-events-auto"
+              : "opacity-0 pointer-events-none"
+          )}
+        >
+          <div className="flex flex-col space-y-8 text-xl">
+            {navItems.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-forground/80 hover:text-primary transition-colors duration-300"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import Navbar from "../Components/Navbar";
 import StarBackground from "../Components/StarBackground";
 import ThemeToggle from "../Components/ThemeToggle";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="min-h-screen bg-background text-forground overflow-x-hidden">
       <ThemeToggle />
       <StarBackground />
+      <Navbar />
     </div>
   );
 }
