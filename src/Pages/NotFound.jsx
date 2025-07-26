c0cf2b63a97aeae3d31d2071f0588c1324bd4dc8
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-07-26 13:42:57 +0430

    Update NotFound.jsx

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,3 +1,7 @@
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <p className="flex flex-col text-xl">
+      <span className="text-5xl font-bold mb-2">404!</span>Page Not Found
+    </p>
+  );
 }
